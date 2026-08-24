e64aa3c008da65b761316b425d28af5ee63f152d
kuteyisamuel16-bit <kuteyisamuel16@gmail.com>
2026-08-24 18:32:59 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,7 +3,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=280,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
